8e60cd1c32f40ed7d0e47cee82e04d294a38e35a
Mohamed Yassin <mohgamalyassin@outlook.com>
2021-12-26 18:18:55 +0200

    first commit2

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-         path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('polls/owner', views.owner, name='owner'),
-    path('polls/<int:question_id>/', views.detail, name='detail'),
-    path('polls/<int:question_id>/results/', views.results, name='results'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 
     ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.utils.regex_helper import Choice
+from django.views import generic
+
 from . import models
 
 
@@ -20,8 +24,42 @@ def detail(request, question_id):
 
 def results(request, question_id):
     choices = models.Choice.objects.filter(question_id=question_id)
-    return render(request, 'main/choices.html', context={"choices":choices})
+    return render(request, 'main/choices.html', context={"choices": choices})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(models.Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'main/details.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = 'main/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return models.Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = models.Question
+    template_name = 'main/details.html'
+
+
+class ResultsView(generic.DetailView):
+    model = models.Question
+    template_name = 'main/results.html'
